f9b4c39a7a5faa0a90dc0252e8b900c7e26d40d0
Alex Rokachev <alserokachev@gmail.com>
2023-01-18 19:22:09 +0300

    made ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+System.Console.WriteLine("Введите число: ");
+string stringNumb = Console.ReadLine();
+int numb = Convert.ToInt32(stringNumb);
+int numbLength = stringNumb.Length;
+int answer = numb;
+int divider = 10;
+
+if (numbLength > 2)
+{
+    if (numbLength > 3)
+    {
+        while (answer > 1000)
+        {
+            answer = numb / divider;
+            divider = divider * 10;
+        }
+        answer = answer % 10;
+        System.Console.WriteLine($"Ответ: {answer}");
+    }
+    else
+    {
+        System.Console.WriteLine($"Ответ: {numb % 10}");
+    }
+}
+else
+{
+    System.Console.WriteLine("Некорректное число");
+}
+
+
+
+
+// System.Console.Write("Введите число: ");
+// string numb = Console.ReadLine();
+// int numbLength = numb.Length;
+
+// if(numbLength > 2) {
+//     System.Console.WriteLine($"Это число: {numb[2]}");
+// } else {
+//     System.Console.WriteLine("Некорректное число");
+// }
